57eb99d2aa21d4f51dcc59854d915fac85fa90f9
Inger Mathilde Eiklund <ingeik06345@stud.noroff.no>
2026-05-11 11:33:55 +0200

    created checkout and successpage

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,12 +1,33 @@
 import { renderOrderSummary } from "../cart/cart.js";
+import { toastNotification } from "../messages.js";
 
 const cart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
 const summaryWrapper = document.getElementById("summary-wrapper");
 
 if (summaryWrapper) {
   renderOrderSummary(cart, summaryWrapper);
+
   const payOrderBtn = document.getElementById("pay-order-button");
-  payOrderBtn.addEventListener("click", () => {
+  const detailsForm = document.getElementById("details-wrapper");
+  detailsForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const detailInputs = document.querySelectorAll(".details-input");
+    let isFormValid = true;
+    console.log(detailInputs);
+
+    detailInputs.forEach((input) => {
+      if (!input.value.trim()) {
+        input.classList.add("empty-input");
+        isFormValid = false;
+      } else {
+        input.classList.remove("empty-input");
+      }
+    });
+    if (!isFormValid) {
+      toastNotification("Fields cannot be empty!", "warning", 2);
+      return;
+    }
     window.location.href = "/success/index.html";
   });
 }
