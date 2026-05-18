f26783f40b9fbe8f688c23da621688365f93ffa4
Inger Mathilde Eiklund <ingeik06345@stud.noroff.no>
2026-05-18 10:09:33 +0200

    trying some more

--- header.js
+++ header.js
@@ -22,7 +22,7 @@ function HamburgerMenu() {
         closeMenu();
         if (localStorage.getItem("access_token") === null) {
           e.preventDefault();
-          window.location.href = "../account/login.html";
+          window.location.href = "./account/login.html?redirected=user";
           // window.location.href = userLink.href.replace(
           //   /account\/user\.html/,
           //   "account/login.html?redirected=user",
@@ -41,13 +41,13 @@ function HamburgerMenu() {
         closeMenu();
         if (localStorage.getItem("access_token") === null) {
           e.preventDefault();
-          window.location.href = "../account/login.html";
+          window.location.href = "./account/login.html?redirected=cart";
           // window.location.href = shoppingcartLink.href.replace(
           //   /cart\/index.html/,
           //   "account/login.html?redirected=cart",
           // );
         } else {
-          window.location.href = shoppingcartLink.href;
+          window.location.href = shoppingcartIcons.href;
         }
       });
     }
